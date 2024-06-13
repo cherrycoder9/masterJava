346bf3552ae2b5799a63f25e5faa6d122f9e0628
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-13 10:21:59 +0900

    연습문제 풀이

---
+++ src/day06/step1/Person.java
@@ -0,0 +1,12 @@
+package day06.step1;
+
+// 1. class
+// 2. 'Person'
+
+public class Person {
+    String name; // 이름
+    int age; // 나이
+    char gender; // 성별
+
+
+}
---
+++ src/day06/step1/붕어빵틀.java
@@ -0,0 +1,7 @@
+package day06.step1;
+
+public class 붕어빵틀 {
+    int 반죽용량;
+    String 속재료;
+    
+}
