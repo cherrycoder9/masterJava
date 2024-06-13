cd9de206d26c173c091fb16ba3c348a8b8167b55
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-13 13:01:31 +0900

    연습문제 풀이

---
+++ src/day06/step3/Phone.java
@@ -0,0 +1,14 @@
+package day06.step3;
+
+// 추상적으로 생각한 것을 구체적(속성/행위)으로 설계도를 작성한 작업: 클래스 정의
+// 객체를 설계한 설계도
+public class Phone {
+    // 필드
+    String name; // 폰 모델명
+    String weight; // 폰 무게
+
+    // 메소드
+    public void 전화걸기() {
+    }
+}
+// 객체를 설계한 설계도
---
+++ src/day06/step4/Step4.java
@@ -0,0 +1,7 @@
+package day06.step4;
+
+public class Step4 {
+    public static void main(String[] args) {
+        
+    }
+}
