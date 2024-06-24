d4dda2b925e4abbfabfe14246ff81f81a2021a5b
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-24 11:42:37 +0900

    1교시 종료

--- src/dataBase/4_DML기초.sql
+++ src/dataBase/4_DML기초.sql
@@ -58,14 +58,117 @@ where mname = '유재석';
 
 # [3] UPDATE : 레코드 (행, 튜플, 가로단위)의 필드 값 수정 
 # 1.  특정 필드의 전체 값 수정: update 테이블명 set 수정할필드명 = 새로운값;
-update table1 set mno = 10; # 테이블의 mno필드를 10으로 수정
+update table1
+set mno = 10;
+# 테이블의 mno필드를 10으로 수정
 
 # 2. 특정 필드의 조건에 따른 값 수정 
-update table1 set mno = 3 where mname = '유재석';
-update table1 set mname = '유재석B' where mno = 3;
+update table1
+set mno = 3
+where mname = '유재석';
+update table1
+set mname = '유재석B'
+where mno = 3;
 
-select * from table1;
+select *
+from table1;
 
 # update, delete는 workbench에서 기본적으로 차단함 -> safe mode
 # safe mode 끄기
-SET SQL_SAFE_UPDATES = 0;  # safe mode = 0: 해제, 1: 사용
+SET SQL_SAFE_UPDATES = 0;
+# safe mode = 0: 해제, 1: 사용
+
+# [4] DELTEL: 레코드(행, 튜플, 가로단위) 삭제
+# 2. 특정 조건에 따른 레코드 삭제: delete from 테이블명 where 조건절
+
+# 테이블의 mno 필드가 3인 레코드를 삭제
+delete
+from table1
+where mno = 3;
+# 테이블의 mname 필드가 '강호동'인 레코드를 삭제
+delete
+from table1
+where mname = '강호동';
+
+# 1. 전체 레코드 삭제: delete from 테이블명;
+# 1번은 실무에서 잘 안쓴다
+# 느리다, 취소 가능
+delete
+from table1;
+# 빠르다, 취소불가능
+truncate table table1;
+
+/*
+* 테이블이 있다는 가정하에 SQL만 작성, 풀이후 (테스트)실행 불가능
+[문제1]: employees 테이블에 새로운 직원을 추가하세요. 직원의 정보는 다음과 같습니다:
+employee_id: 101
+first_name: 'John'
+last_name: 'Doe'
+email: 'johndoe@example.com'
+hire_date: '2024-06-24'
+job_id: 'IT_PROG'
+
+[문제2]: products 테이블에 새로운 제품을 추가하세요. 제품의 정보는 다음과 같습니다:
+product_id: 201
+product_name: 'Wireless Mouse'
+category: 'Electronics'
+price: 29.99
+stock_quantity: 150
+
+[문제3]: employees 테이블에서 모든 직원의 first_name과 email을 조회하세요.
+
+[문제4]: products 테이블에서 price가 1000 인 제품의 product_name과 price를 조회하세요.
+
+[문제5]: employees 테이블에서 employee_id가 101인 직원의 email을 'john.doe@company.com'으로 업데이트하세요.
+
+[문제6]: products 테이블에서 product_id가 201인 제품의 price를 24.99로 업데이트하세요.
+
+[문제7]: employees 테이블에서 employee_id가 101인 직원을 삭제하세요.
+
+[문제8]: products 테이블에서 product_id가 201인 제품을 삭제하세요.
+
+*/
+
+create table employees(
+employee_id int, 
+first_name varchar(30), 
+last_name varchar(30), 
+email varchar(30),
+hire_date varchar(30), 
+job_id varchar(30)
+);
+# 문제1
+insert into employees(employee_id, first_name, last_name, email, hire_date, job_id)
+values (101,'John', 'Doe', 'johndoe@example.com', '2024-06-24', 'IT_PROG');
+
+create table products(
+product_id int, 
+product_name varchar(30), 
+category varchar(30), 
+price double,
+stock_quantity int
+);
+# 문제2
+insert into products(product_id, product_name, category, price, stock_quantity)
+values (201, 'Wireless Mouse', 'Electronics', 1000.0, 150);
+
+# 문제3
+select first_name, email from employees;
+
+# 문제4 
+select product_name, price from employees where price = 1000;
+
+# 문제5
+update employees set email = 'john.doe@company.com' where employee_id = 101;
+
+# 문제6 
+update products set price = 24.99 where product_id = 201;
+
+# 문제7
+delete from employees where employee_id = 101;
+
+# 문제8
+delete from products where product_id = 201;
+
+select * from employees;
+select * from products;
