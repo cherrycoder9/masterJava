004f034b273fb6d961e607ffddc7f89d49467573
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-25 14:33:46 +0900

    점심시간 종료

--- src/day14/step3/Step3.java
+++ src/day14/step3/Step3.java
@@ -1,27 +1,31 @@
+// 메소드 영역: 클래스 정보 저장
 package day14.step3;
 
 public class Step3 {
+    // 메소드 영역: main 메소드 바이트코드 저장
     public static void main(String[] args) {
-        // 인터페이스 타입의 변수 선언
+        // 스택 영역: 지역 변수 si 선언
         StepInterface si;
-        // 인터페이스는 스스로 객체를 생성 불가능
-        // 구현 안한 메소드가 있으니까 --> 추상메소드
-        // 구현을 해줄 클래스가 필요하다ㅏ
-        // implements 한 클래스로 객체 생성한다 --> 구현(객)체
-        new ImplementClass(); // (인스턴스 구현된 클래스의) 객체
+
+        // 힙 영역: ImplementClass 객체 생성
+        new ImplementClass();
+        // 스택 영역: si에 객체 참조 저장
         si = new ImplementClass();
+
+        // 스택 영역: 메소드 호출 시 스택 프레임 생성
         si.method1(3);
         si.method2("3");
         si.method3(3);
         si.method4("3");
-        // si.method5(5); // static 객체 없이 사용하는 멤버들
-        // si.method6("3"); // static 객체 없이 사용하는 멤버들
+
+        // 메소드 영역의 static 메소드 호출
         StepInterface.method5(5);
         StepInterface.method6("5");
-        // 인터페이스 안에서 내부적으로 사용할때 private 사용해야함
-        // si.method7(3); // private이므로 외부에서 사용 불가능
-        // si.method8("hi"); // private이므로 외부에서 사용 불가능
-        // si.method9(3); // private
-        // si.method10("3"); // private
+
+        // private 메소드는 외부에서 접근 불가
+        // si.method7(3);
+        // si.method8("hi");
+        // si.method9(3);
+        // si.method10("3");
     }
 }
--- src/day14/step3/StepInterface.java
+++ src/day14/step3/StepInterface.java
@@ -1,11 +1,12 @@
+// 메소드 영역: 인터페이스 정보 저장
 package day14.step3;
 
 public interface StepInterface {
-    // 1. 상수
+    // 메소드 영역: static final 상수
     public static final int value = 10;
     int value2 = 20; // 인터페이스 안에서는 변수 생성시 무조건 상수
 
-    // 4. 정적메소드
+    // 메소드 영역: static 메소드
     static void method5(int a) {
         System.out.println("StepInterface.method5");
     }
@@ -15,7 +16,7 @@ public interface StepInterface {
         return 3;
     }
 
-    // 6. private static 메소드
+    // 메소드 영역: private static 메소드
     private static void method9(int a) {
         System.out.println("StepInterface.method9");
     }
@@ -25,12 +26,12 @@ public interface StepInterface {
         return 3;
     }
 
-    // 2. 추상메소드
-    abstract void method1(int a); // 구현을 안한 추상메소드
+    // 메소드 영역: 추상메소드 선언
+    abstract void method1(int a);
 
     int method2(String a);
 
-    // 3. 디폴트메소드
+    // 메소드 영역: 디폴트메소드
     default void method3(int a) {
         System.out.println("StepInterface.method2");
     }
@@ -40,7 +41,7 @@ public interface StepInterface {
         return 3;
     }
 
-    // 5. private 메소드
+    // 메소드 영역: private 메소드
     private void method7(int a) {
         System.out.println("StepInterface.method7");
     }
