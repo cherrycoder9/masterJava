ebf3a8e0449641553e9c70e6dfe4d5e4cfa4d33a
cherrycoder9 <cherrycoder9@gmail.com>
2024-06-28 15:04:43 +0900

    5교시중, 주석다는중

---
+++ src/test/Test2.java
@@ -0,0 +1,34 @@
+package test;
+
+// 부모 클래스
+class Parent {
+    public void parentMethod() {
+        System.out.println("Parent method");
+    }
+}
+
+// 자식 클래스
+class Child extends Parent {
+    public void childMethod() {
+        System.out.println("Child method");
+    }
+}
+
+// 손자 클래스
+class GrandChild extends Child {
+    public void grandChildMethod() {
+        System.out.println("GrandChild method");
+    }
+}
+
+public class Test2 {
+    public static void main(String[] args) {
+        // 손자 객체 생성
+        GrandChild grandChild = new GrandChild();
+
+        // 손자 객체를 통해 부모, 자식, 손자 메서드 호출
+        grandChild.parentMethod(); // 부모 클래스의 메서드
+        grandChild.childMethod();  // 자식 클래스의 메서드
+        grandChild.grandChildMethod(); // 손자 클래스의 메서드
+    }
+}
